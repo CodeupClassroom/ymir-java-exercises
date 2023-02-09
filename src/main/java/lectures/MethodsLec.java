2bf41c8f2b8db24cbca1ebc22717d2fb6fdbcf67
Docrob <mark.robinson@codeup.com>
2023-02-09 10:42:13 -0600

    pre method lec

---
+++ src/main/java/lectures/MethodsLec.java
@@ -0,0 +1,30 @@
+package lectures;
+
+public class MethodsLec {
+
+    public static void main(String[] args) {
+
+        // technical diff between method and function
+
+        // create a sayHello method with a string param
+            // point out the parts of method signature
+            // remind students of difference in DEFINING and CALLING a method
+
+        // overload with a sayHello method with no parameters
+            // introduce NO ARG term
+
+        // overload with an int to print hello a number of times
+            // show reuse by calling the no arg version
+
+        // talk about passing by value and how to change a parameter's value OUTSIDE of the function
+            // demo string changeMe function
+            // 2 ways to change outside variable:
+                // 1. pass it contained in an object (WILL SHOW NEXT WEEK)
+                // 2. or make it global (show the static shuffle) and advise to be careful
+
+        // fun with recursion!
+            // briefly go over codeup's count example
+            // BRIEFLY use my fun w recursion slides to talk in more depth
+                // maybe show my fibonacci examples
+    }
+}
