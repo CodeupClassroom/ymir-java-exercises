e3ac75c36b59a0aaf94d223f040e6d75c5db1465
Docrob <mark.robinson@codeup.com>
2023-03-05 21:50:41 -0600

    removed old code from screenbuilder

--- src/main/java/adventure/screens/ScreenBuilder.java
+++ src/main/java/adventure/screens/ScreenBuilder.java
@@ -1,21 +1,11 @@
 package adventure.screens;
 
 import adventure.menu.FlowAction;
-
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 
 public class ScreenBuilder {
     // we can save screens in a cache to reuse them later
     private static final HashMap<String, Screen> screenCache = new HashMap<>();
-
-//    public static FlowAction buildScreen(Class screenClass) {
-//        Screen nextScreen = getCachedScreen(screenClass);
-//        ScreenManager.setNextScreen(nextScreen);
-//        return () -> {
-//
-//        };
-//    }
 
     public static FlowAction buildScreen(Screen screen) {
         Screen nextScreen = getCachedScreen(screen);
@@ -24,15 +14,6 @@ public class ScreenBuilder {
         };
     }
 
-    //    public static Screen buildScreen(ScreenType screenType) {
-//        return switch(screenType) {
-//            case Welcome -> new WelcomeScreen();
-//            case Main -> new MainScreen();
-//            case Town -> getCachedScreen(screenType, new TownScreen());
-//            case DungeonEntrance -> getCachedScreen(screenType, new DungeonEntrance());
-//            default -> new ErrorScreen();
-//        };
-//    }
     private static Screen getCachedScreen(Screen screen) {
         String screenClassName = screen.getClass().getSimpleName();
         if(!screenCache.containsKey(screenClassName)) {
@@ -41,31 +22,4 @@ public class ScreenBuilder {
         }
         return screenCache.get(screenClassName);
     }
-
-    private static Screen getCachedScreen(Class<Screen> screenClass) {
-        String screenClassName = screenClass.getSimpleName();
-        if(!screenCache.containsKey(screenClassName)) {
-            try {
-                Screen screen = screenClass.getDeclaredConstructor().newInstance();
-                screenCache.put(screenClassName, screen);
-            } catch (NoSuchMethodException | InstantiationException | IllegalAccessException |
-                     InvocationTargetException e) {
-                throw new RuntimeException(e);
-            }
-        }
-//            try {
-//            } catch (InstantiationException | IllegalAccessException | NoSuchMethodException |
-//                     InvocationTargetException e) {
-//                throw new RuntimeException(e);
-//            }
-//        }
-        return screenCache.get(screenClassName);
-    }
-
-//    private static Screen getCachedScreen(ScreenType screenType, Screen newScreen) {
-//        if(!screenCache.containsKey(screenType)) {
-//            screenCache.put(screenType, newScreen);
-//        }
-//        return screenCache.get(screenType);
-//    }
 }
